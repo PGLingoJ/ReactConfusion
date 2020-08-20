050e31a3449756a50e200d38e75fe3a9547079c5
PGLingoJ <lingo.jl@pg.com>
2020-08-20 09:49:15 -0400

    Assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishdetailComponent from "./DishdetailComponent";
 
 
 class Menu extends Component {
@@ -17,17 +18,9 @@ class Menu extends Component {
 
     renderDish(dish) {
         if (dish != null){
-            return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
+            return <DishdetailComponent dish={dish} />
         } else {
-            return(<div></div>);
+            return <div></div>
         }
     }
 
@@ -47,12 +40,8 @@ class Menu extends Component {
 
         return (
           <div className="container">
-            <div className="row">
-                {menu}
-            </div>
-            <div className="row">
-                {this.renderDish(this.state.selectedDish)}
-            </div>
+            <div className="row">{menu}</div>
+            {this.renderDish(this.state.selectedDish)}
           </div>
         );
     }
